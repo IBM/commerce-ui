05ea7affec6178cc0f7526cc0fd600fc2a245853
Shiqi Yi <shiqiyi@ibm.com>
2019-01-30 10:56:10 -0500

    remove duplicated confidential info

--- commerce-management-fixture/src/main/java/com/ibm/commerce/cmc/ui/page/impl/AbstractCMCPage.java
+++ commerce-management-fixture/src/main/java/com/ibm/commerce/cmc/ui/page/impl/AbstractCMCPage.java
@@ -20,23 +20,6 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.util.List;
-
-/*
- *-----------------------------------------------------------------
- * IBM Confidential
- *
- * OCO Source Materials
- *
- * WebSphere Commerce
- *
- * (C) Copyright IBM Corp. 2012, 2015
- *
- * The source code for this program is not published or otherwise
- * divested of its trade secrets, irrespective of what has
- * been deposited with the U.S. Copyright Office.
- *-----------------------------------------------------------------
- */
-
 import java.util.logging.Logger;
 
 import com.google.inject.Inject;
