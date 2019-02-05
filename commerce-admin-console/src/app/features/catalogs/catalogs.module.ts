6b191c018eca01807ca96ae87743bb1ac92507ab
charan kumar <charankumar@charans-MacBook-Pro.local>
2019-02-05 18:09:38 +0530

    search component with some ui changes

--- commerce-admin-console/src/app/features/catalogs/catalogs.module.ts
+++ commerce-admin-console/src/app/features/catalogs/catalogs.module.ts
@@ -5,7 +5,7 @@ import { CatalogsRoutingModule } from './catalogs-routing.module';
 import { IconModule } from '@carbon/icons-angular';
 import { ButtonModule,
     TabsModule, AccordionModule, CheckboxModule, RadioModule, TableModule, DropdownModule,
-LinkModule, ModalModule, SearchModule, PlaceholderModule, DialogModule, UIShellModule  } from 'carbon-components-angular';
+LinkModule, ModalModule, SearchModule, PlaceholderModule, DialogModule, UIShellModule, FileUploaderModule, NotificationModule  } from 'carbon-components-angular';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
@@ -19,6 +19,12 @@ import { ModalService } from 'carbon-components-angular';
 import { UnderconstructionComponent } from './components/underconstruction/underconstruction.component';
 import { UiShellComponent } from './components/ui-shell/ui-shell.component'
 import { MainUiShellComponent } from './components/main-ui-shell/main-ui-shell.component';
+import { StockHolmCASComponent } from './components/stock-holm-cas/stock-holm-cas.component';
+import { StockholmComponent } from './components/stockholm/stockholm.component';
+import { UnassignedCatEntriesComponent } from './components/unassigned-cat-entries/unassigned-cat-entries.component';
+import { CatalogUploadComponent } from './components/catalog-upload/catalog-upload.component';
+import { NewCatalogUploadComponent } from './components/new-catalog-upload/new-catalog-upload.component';
+import { CatalogLevelSkuComponent } from './components/catalog-level-sku/catalog-level-sku.component'
 
 @NgModule({
 imports: [
@@ -39,11 +45,14 @@ SearchModule,
 PlaceholderModule,
 DialogModule,
 IconModule,
-UIShellModule
+UIShellModule,
+FileUploaderModule,
+NotificationModule
 ],
 declarations: [HeaderComponent, FooterComponent, SideBarComponent, ModelPopupComponent,
 NewMasterCategoryComponent, ExtendedSitesComponent, LandingPageComponent, SearchResultComponent, UiShellComponent, UnderconstructionComponent,
-MainUiShellComponent],
+StockholmComponent, MainUiShellComponent, StockHolmCASComponent, UnassignedCatEntriesComponent, CatalogUploadComponent, NewCatalogUploadComponent, 
+CatalogLevelSkuComponent],
 
 
 providers: [ModalService],
