e4779929c791ab68a90c6936def86b9345f0d9ad
Asutosh Roy <asutoroy@in.ibm.com>
2019-02-05 13:08:30 +0530

    Translation changes in new master category and landing page components

--- commerce-admin-console/src/app/features/catalogs/components/new-master-category/new-master-category.component.ts
+++ commerce-admin-console/src/app/features/catalogs/components/new-master-category/new-master-category.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, ViewChild, TemplateRef } from '@angular/core';
 import {TableModel, TableHeaderItem, TableItem } from 'carbon-components-angular';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 import { ViewEncapsulation } from '@angular/compiler/src/core';
 
 @Component({
@@ -29,6 +31,7 @@ protected customTableItemTemplate: TemplateRef<any>;
   pageTitle = '';
   metaDesc = '';
   imgAltText = '';
+  disableInput = true;
   showAssociate: boolean;
   showContent: boolean;
   showReferences: boolean;
@@ -37,7 +40,77 @@ protected customTableItemTemplate: TemplateRef<any>;
   showReferencesEdit: boolean;
   masterInputData = [];
 
-  constructor() { }
+  constructor(private translate: TranslateService) { }
+
+  private readonly headerIndex = {
+    '0': {
+      'translateId': 'CATALOGS.NEW_MASTER_CATEGORY.ASSOCIATE_TABLE.sequence',
+    },
+    '1': {
+      'translateId': 'CATALOGS.NEW_MASTER_CATEGORY.ASSOCIATE_TABLE.type',
+    },
+    '2': {
+      'translateId': 'CATALOGS.NEW_MASTER_CATEGORY.ASSOCIATE_TABLE.store',
+    },
+    '3': {
+      'translateId': 'CATALOGS.NEW_MASTER_CATEGORY.ASSOCIATE_TABLE.path',
+    },
+    '4': {
+      'translateId': 'CATALOGS.NEW_MASTER_CATEGORY.ASSOCIATE_TABLE.code',
+    },
+    '5': {
+      'translateId': 'CATALOGS.NEW_MASTER_CATEGORY.ASSOCIATE_TABLE.usage',
+    },
+    '6': {
+      'translateId': 'CATALOGS.NEW_MASTER_CATEGORY.ASSOCIATE_TABLE.administative_name',
+    },
+    '7': {
+      'translateId': 'CATALOGS.NEW_MASTER_CATEGORY.ASSOCIATE_TABLE.display_name',
+    },
+    '8': {
+      'translateId': 'CATALOGS.NEW_MASTER_CATEGORY.ASSOCIATE_TABLE.short_description',
+    }
+  };
+  listItems = [{
+    content: 'Show All',
+    selected: false
+      },
+      {
+    content: 'AuroraESite',
+    selected: false,
+      },
+      {
+    content: 'ExtendedSitesCatalogAssetStore',
+          selected: false
+      }
+    ];
+
+  private translationSubscription: Subscription;
+
+  populateTableHeader() {
+    this.translationSubscription = this.translate.get([this.headerIndex['0'].translateId,
+    this.headerIndex['1'].translateId,
+    this.headerIndex['2'].translateId,
+    this.headerIndex['3'].translateId,
+    this.headerIndex['4'].translateId,
+    this.headerIndex['5'].translateId,
+    this.headerIndex['6'].translateId,
+    this.headerIndex['7'].translateId,
+    this.headerIndex['8'].translateId
+    ]).subscribe((result: object) => {
+      this.customModel.header = [
+        new TableHeaderItem({ data: result[this.headerIndex['0'].translateId] }),
+        new TableHeaderItem({ data: result[this.headerIndex['1'].translateId] }),
+        new TableHeaderItem({ data: result[this.headerIndex['2'].translateId] }),
+        new TableHeaderItem({ data: result[this.headerIndex['3'].translateId] }),
+        new TableHeaderItem({ data: result[this.headerIndex['4'].translateId] }),
+        new TableHeaderItem({ data: result[this.headerIndex['5'].translateId] }),
+        new TableHeaderItem({ data: result[this.headerIndex['6'].translateId] }),
+        new TableHeaderItem({ data: result[this.headerIndex['7'].translateId] }),
+        new TableHeaderItem({ data: result[this.headerIndex['8'].translateId] })
+      ];
+    });
+  }
 
     saveMasterData() {
       this.masterInputData = [
@@ -52,19 +125,6 @@ protected customTableItemTemplate: TemplateRef<any>;
   // closeMasterCategory() {
   // this.router.navigate(['/']);
   // }
-addRow() {
-  this.customModel.data = [
-    [new TableItem({data: '', template: this.customTableItemTemplate}),
-    new TableItem({data: ''}),
-    new TableItem({data: '', template: this.customTableItemTemplate}),
-    new TableItem({data: '', template: this.customTableItemTemplate}),
-    new TableItem({data: '', template: this.customTableItemTemplate}),
-    new TableItem({data: '', template: this.customTableItemTemplate}),
-    new TableItem({data: '', template: this.customTableItemTemplate}),
-    new TableItem({data: '', template: this.customTableItemTemplate}),
-    new TableItem({data: '', template: this.customTableItemTemplate})]
-  ];
-}
   ngOnInit() {
     this.showAssociate = true;
     this.showContent = true;
@@ -72,43 +132,79 @@ addRow() {
     this.showAssociateEdit = false;
     this.showContentEdit = false;
     this.showReferencesEdit = false;
-      this.customModel.header = [
-        new TableHeaderItem({ data: 'Sequence'}),
-        new TableHeaderItem({ data: '*Type'}),
-        new TableHeaderItem({ data: 'Store'}),
-        new TableHeaderItem({ data: 'Path'}),
-        new TableHeaderItem({ data: 'Code'}),
-        new TableHeaderItem({ data: 'Usage'}),
-        new TableHeaderItem({ data: 'Administative name (United State English)'}),
-        new TableHeaderItem({ data: 'Display name (United State English)'}),
-        new TableHeaderItem({ data: 'Short description (United State English)'})
-      ];
+
     //   this.customModel.data = [
     //     [new TableItem({data: ''}), new TableItem({data: ''}), new TableItem({data: ''}), new TableItem({data: ''}),
     //     new TableItem({data: ''}), new TableItem({data: ''}), new TableItem({data: ''}), new TableItem({data: ''}),
     //     new TableItem({data: ''})]
     //   ];
-  this.addRow();
   }
 
     closeMasterCategory() { }
-    addAssociateRow() {
-        this.showAssociate = false;
-        this.showAssociateEdit = true;
-        this.addRow();
-    }
-    addContentRow() {
-      this.showContent = false;
-      this.showContentEdit = true;
-      this.addRow();
-    }
-    addReferencesRow() {
-      this.showReferences = false;
-      this.showReferencesEdit = true;
-      this.addRow();
-    }
     deleteNewRow() {
         console.log('hello');
     }
-
+    associateClicked() {
+      // this.customModel.header = [
+      //   new TableHeaderItem({ data: 'Sequence'}),
+      //   new TableHeaderItem({ data: '*Type'}),
+      //   new TableHeaderItem({ data: 'Store', template: this.customHeaderTemplate}),
+      //   new TableHeaderItem({ data: 'Path'}),
+      //   new TableHeaderItem({ data: 'Code'}),
+      //   new TableHeaderItem({ data: 'Usage'}),
+      //   new TableHeaderItem({ data: 'Administative name (United State English)'}),
+      //   new TableHeaderItem({ data: 'Display name (United State English)'}),
+      //   new TableHeaderItem({ data: 'Short description (United State English)'})
+      // ];
+      this.populateTableHeader();
+      this.customModel.data = [
+        [new TableItem({data: '', template: this.customTableItemTemplate}),
+        new TableItem({data: ''}),
+        new TableItem({data: '', template: this.customTableItemTemplate}),
+        new TableItem({data: '', template: this.customTableItemTemplate}),
+        new TableItem({data: '', template: this.customTableItemTemplate}),
+        new TableItem({data: '', template: this.customTableItemTemplate}),
+        new TableItem({data: '', template: this.customTableItemTemplate}),
+        new TableItem({data: '', template: this.customTableItemTemplate}),
+        new TableItem({data: '', template: this.customTableItemTemplate})]
+      ];
+    }
+    contentClicked() {
+      this.customModel.header = [
+        new TableHeaderItem({ data: 'Language'}),
+        new TableHeaderItem({ data: 'Identifier'}),
+        new TableHeaderItem({ data: 'Name'}),
+        new TableHeaderItem({ data: 'Content Type'}),
+        new TableHeaderItem({ data: 'Description'})
+      ];
+      this.customModel.data = [
+        [new TableItem({data: ''}),
+        new TableItem({data: ''}),
+        new TableItem({data: ''}),
+        new TableItem({data: ''}),
+        new TableItem({data: ''})]
+      ];
+    }
+    referencesClicked() {
+      this.customModel.header = [
+        new TableHeaderItem({ data: '*Type'}),
+        new TableHeaderItem({ data: 'Status'}),
+        new TableHeaderItem({ data: 'Store'}),
+        new TableHeaderItem({ data: 'Name'}),
+        new TableHeaderItem({ data: 'Description'}),
+        new TableHeaderItem({ data: 'Start Date'}),
+        new TableHeaderItem({ data: 'End Date'}),
+        new TableHeaderItem({ data: 'Campaign'})
+      ];
+      this.customModel.data = [
+        [new TableItem({data: ''}),
+        new TableItem({data: ''}),
+        new TableItem({data: ''}),
+        new TableItem({data: ''}),
+        new TableItem({data: ''}),
+        new TableItem({data: ''}),
+        new TableItem({data: ''}),
+        new TableItem({data: ''})]
+      ];
+    }
 }
