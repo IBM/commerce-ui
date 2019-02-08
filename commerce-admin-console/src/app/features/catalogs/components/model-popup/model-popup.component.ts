65531ac77424507e989d3149023ff0e633475e3d
Naresh  Reddy Gadi <naresh@Nareshs-MacBook-Pro.local>
2019-02-08 17:12:18 +0530

    Updated Footer and Modal components

--- commerce-admin-console/src/app/features/catalogs/components/model-popup/model-popup.component.ts
+++ commerce-admin-console/src/app/features/catalogs/components/model-popup/model-popup.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit,Output,EventEmitter, Input, ViewEncapsulation } from '@angular/core';
 import {  Modal } from 'carbon-components-angular';
 import { TableModel, TableItem, TableHeaderItem, DialogModule } from 'carbon-components-angular';
-
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'ac-model-popup',
@@ -11,12 +12,24 @@ import { TableModel, TableItem, TableHeaderItem, DialogModule } from 'carbon-com
 })
 export class ModelPopupComponent implements OnInit {
 
-  @Input() model = new TableModel();
+  @Input() customModel = new TableModel();
   @Input() size = 'md';
-  @Input() showSelectionColumn = true;
+  @Input() showSelectionColumn = false;
   @Input() striped = true;
   availableRows = 0;
   totalRows = 0;
+  private readonly headerIndex = {
+      '0': {
+        'translateId': 'CATALOGS.FOOTR.message'
+      },
+      '1': {
+        'translateId': 'CATALOGS.FOOTR.object'
+      },
+      '2': {
+        'translateId': 'CATALOGS.FOOTR.date_and_time'
+      }
+    }
+
   @Input() modalListitems = [
     {
       content: 'Show All',
@@ -40,18 +53,32 @@ export class ModelPopupComponent implements OnInit {
 	closeModal(event: any): void {
 		this.close.emit();
 	}
-
-  constructor() { }
-
+ 
+  constructor(private translate: TranslateService) { }
+  private translationSubscription: Subscription;
   ngOnInit() {
-  
-  this.model.header = [
-    new TableHeaderItem({ data: "Message" }),
-    new TableHeaderItem({ data: "Object" }),
-    new TableHeaderItem({ data: "Date and Time" }),
-  ];
-  // this.model.data = [
-  //   [new TableItem({data: " "}), new TableItem({data: " "}), new TableItem({data: " "})]
-  // ];
+    this.populateTableHeader();
+    this.customModel.data = [
+      [new TableItem({data: ''}),
+      new TableItem({data: ''}),
+      new TableItem({data: ''})]
+    ];
+
+    // this.customModel.data = [
+    //   [new TableItem({data: ""}),new TableItem({data: " "}), new TableItem({data: " "})]
+    // ];
+
+}
+populateTableHeader() {
+  this.translationSubscription = this.translate.get([this.headerIndex['0'].translateId,
+  this.headerIndex['1'].translateId,
+  this.headerIndex['2'].translateId
+  ]).subscribe((result: object) => {
+    this.customModel.header = [
+      new TableHeaderItem({ data: result[this.headerIndex['0'].translateId] }),
+      new TableHeaderItem({ data: result[this.headerIndex['1'].translateId] }),
+      new TableHeaderItem({ data: result[this.headerIndex['2'].translateId] })
+    ];
+  });
 }
 }
