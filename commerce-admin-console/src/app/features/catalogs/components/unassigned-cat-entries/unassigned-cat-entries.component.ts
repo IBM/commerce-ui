f542cc2c40b6321f0e44c4712acef5938ac6142d
Naresh  Reddy Gadi <naresh@Nareshs-MacBook-Pro.local>
2019-02-14 19:06:49 +0530

    Added Unassigned categoreis Component

---
+++ commerce-admin-console/src/app/features/catalogs/components/unassigned-cat-entries/unassigned-cat-entries.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-unassigned-cat-entries',
+  templateUrl: './unassigned-cat-entries.component.html',
+  styleUrls: ['./unassigned-cat-entries.component.scss']
+})
+export class UnassignedCatEntriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
