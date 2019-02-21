1fb81a0c2885cafa007a5fce02e67822ee8ecf9e
Mohan kumar L A <mohankumarl@hcl.com>
2019-02-21 16:48:51 +0530

    loading the components based on store selection

--- commerce-admin-console/src/app/features/catalogs/services/storeSelection/storeselection.service.ts
+++ commerce-admin-console/src/app/features/catalogs/services/storeSelection/storeselection.service.ts
@@ -5,16 +5,63 @@ import { Injectable } from '@angular/core';
 })
 export class StoreselectionService {
 
-  initialLoad:boolean = true;
+  navToStockCAS:boolean = false;
   navToMaster:boolean = false;
+  navToStock:boolean= false;
+  navToUnassigned:boolean = false;
+  navToExtendedSite:boolean = false;
+  navigateToCatalogUpload:boolean=false;
+  navigateToSearch:boolean=false;
+  navigateToCatalogSKU:boolean=false;
 
   set(){
-    debugger;
+    //debugger;
     this.navToMaster = true;
   }
   get(){
    return this.navToMaster;
   }
-
+  setStockCAS(){
+    this.navToStockCAS = true;
+  }
+  getStockCAS(){
+   return this.navToStockCAS;
+  }
+  setStock(){
+    this.navToStock = true;
+  }
+  getStock(){
+   return this.navToStock;
+  }
+  setUnassignedCatEntries(){
+    this.navToUnassigned = true;
+  }
+  getUnassignedCatEntries(){
+    return this.navToUnassigned;
+  }
+  setExtendedSite(){
+    this.navToExtendedSite = true;
+  }
+  getExtendedSite(){
+    return this.navToExtendedSite;
+  }
+  setSearch(){
+    this.navigateToSearch=true;
+  }
+  getSearch(){
+    return this.navigateToSearch;
+  }
+  setCatalogUpload(){
+    this.navigateToCatalogUpload=true;
+  }
+  getCatalogUpload(){
+    return this.navigateToCatalogUpload;
+  }
+  setCatalogSku(){
+    this.navigateToCatalogSKU=true;
+  }
+  getCatalogSku(){
+   return this.navigateToCatalogSKU;
+  }
   constructor() { }
 }
