3d88dc74608b2968435ffe9b824c9051417c8a28
Mohan kumar L A <mohankumarl@hcl.com>
2019-02-21 13:16:31 +0530

    added Table for Unassigned-Catalog-Entries component

--- commerce-admin-console/src/app/features/catalogs/catalogs-routing.module.ts
+++ commerce-admin-console/src/app/features/catalogs/catalogs-routing.module.ts
@@ -9,34 +9,38 @@ import { SearchResultComponent } from './components/search-result/search-result.
 import { UnderconstructionComponent } from './components/underconstruction/underconstruction.component';
 import { StockHolmCASComponent } from './components/stock-holm-cas/stock-holm-cas.component';
 import { StockholmComponent } from './components/stockholm/stockholm.component';
+import { UnassignedCatEntriesComponent } from './components/unassigned-cat-entries/unassigned-cat-entries.component'
 
 
 const routes: Routes = [
 {
-path: '', component: LandingPageComponent
+    path: '', component: LandingPageComponent
 },
 {
-path: 'masterCategory', component: NewMasterCategoryComponent
+    path: 'masterCategory', component: NewMasterCategoryComponent
 },
 {
-path: 'searchResult', component: SearchResultComponent
+    path: 'searchResult', component: SearchResultComponent
 },
 {
-path: 'UnderConstruction', component: UnderconstructionComponent
+    path: 'UnderConstruction', component: UnderconstructionComponent
 },
 {
-path: 'extendedSite', component: ExtendedSitesComponent
+    path: 'extendedSite', component: ExtendedSitesComponent
 },
 {
-path: 'stockholmcas', component: StockHolmCASComponent
+    path: 'stockholmcas', component: StockHolmCASComponent
 },
 {
-path: 'stockholm', component: StockholmComponent
+    path: 'stockholm', component: StockholmComponent
 },
+{
+    path: 'unassignedCatEntries', component: UnassignedCatEntriesComponent
+}
 ];
 
 @NgModule({
-imports: [RouterModule.forChild(routes)],
-exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
 export class CatalogsRoutingModule { }
--- commerce-admin-console/src/app/features/catalogs/components/side-bar/side-bar.component.ts
+++ commerce-admin-console/src/app/features/catalogs/components/side-bar/side-bar.component.ts
@@ -13,59 +13,47 @@ export class SideBarComponent implements OnInit {
   sideBar1:boolean=false;
   sideBar2:boolean=false;
   // sideBar3:boolean=false;
-  
  
   constructor(private router: Router, private storesele: StoreselectionService) { }
 
-
-
   ngOnInit() {
-   
     // abc = true;
     this.sideBar=this.storesele.get();
     this.sideBar1=this.storesele.getStockCAS();
     this.sideBar2=this.storesele.getStock();
-    
   }
 
   navigateToExtend(): void {
-
     this.router.navigate(['/catalogs/extendedSite']);
-
   }
   navigateToSearch(): void {
-
     this.router.navigate(['/catalogs/searchResult']);
-
   }
-
   navigateToMaster(): void {
-
     this.router.navigate(['/catalogs/masterCategory']);
-
   }
   navigateToStockCAS(): void {
-
     this.router.navigate(['/catalogs/stockholmcas']);
-
   }
   navigateToStock(): void {
-
     this.router.navigate(['/catalogs/stockholm']);
-
   }
+  navigateToUnassigned(): void {
+    this.router.navigate(['/catalogs/unassignedCatEntries']);
+  }
+
 listItems = [
-{
-content: "Master Catalog",
-selected: false
-},
-{
-content: "Master Catalog Categories",
-selected: false,
-},
-{
-content: "Sales Catalog Categories",
-selected: false
-}
+  {
+    content: "Master Catalog",
+    selected: false
+  },
+  {
+    content: "Master Catalog Categories",
+    selected: false,
+  },
+  {
+    content: "Sales Catalog Categories",
+    selected: false
+  }
 ];
 }
