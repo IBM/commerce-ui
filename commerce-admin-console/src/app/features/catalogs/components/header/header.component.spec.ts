793d3be1b8283ec7675f17c8f792903867ccd000
Naresh  Reddy Gadi <naresh@Nareshs-MacBook-Pro.local>
2019-02-25 17:57:10 +0530

    Intergrated the sideBar and Catalog upload errors

--- commerce-admin-console/src/app/features/catalogs/components/header/header.component.spec.ts
+++ commerce-admin-console/src/app/features/catalogs/components/header/header.component.spec.ts
@@ -1,13 +1,16 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { FormsModule } from '@angular/forms';
+import { StoreselectionService } from '../../services/storeSelection/storeselection.service';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let testBedService: StoreselectionService;
+  let componentService: StoreselectionService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -27,4 +30,47 @@ describe('HeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+});
+
+
+
+
+
+
+    beforeEach(() => {
+
+        // refine the test module by declaring the test component
+        TestBed.configureTestingModule({
+            declarations: [HeaderComponent],
+            providers: [StoreselectionService]
+        });
+
+        // Configure the component with another set of Providers
+        TestBed.overrideComponent(
+            HeaderComponent,
+            { set: { providers: [{ provide: StoreselectionService, useClass: MockStoreselectionService }] } }
+        );
+
+        // create component and test fixture
+        fixture = TestBed.createComponent(HeaderComponent);
+
+        // get test component from the fixture
+        component = fixture.componentInstance;
+
+        // StoreselectionService provided to the TestBed
+        testBedService = TestBed.get(StoreselectionService);
+
+        // StoreselectionService provided by Component, (should return MockStoreselectionService)
+        componentService = fixture.debugElement.injector.get(StoreselectionService);
+    });
+
+    it('Service injected via inject(...) and TestBed.get(...) should be the same instance',
+        inject([StoreselectionService], (injectService: StoreselectionService) => {
+            expect(injectService).toBe(testBedService);
+        })
+    );
+
+    it('Service injected via component should be and instance of MockStoreselectionService', () => {
+        expect(componentService instanceof MockStoreselectionService).toBeTruthy();
+    });
 });
--- commerce-admin-console/src/app/features/catalogs/services/storeSelection/storeselection.service.spec.ts
+++ commerce-admin-console/src/app/features/catalogs/services/storeSelection/storeselection.service.spec.ts
@@ -1,9 +1,27 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { StoreselectionService } from './storeselection.service';
 
+
 describe('StoreselectionService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  let service: StoreselectionService
+  beforeEach((done) => {
+    
+    TestBed.configureTestingModule({
+      imports:[],
+      providers:[StoreselectionService]
+    })
+    done();
+  });
+
+  beforeEach(inject([StoreselectionService], _service =>{
+    service = _service;
+  }));
+
+  it('should instantiate', () => {
+    expect(service).toEqual(jasmine.any(StoreselectionService));
+  });
 
   it('should be created', () => {
     const service: StoreselectionService = TestBed.get(StoreselectionService);
