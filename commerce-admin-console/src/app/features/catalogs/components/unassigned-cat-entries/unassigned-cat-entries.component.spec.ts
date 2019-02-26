890402c38c19ed90b8564aa20ff2ac28e37f39f7
Mohan kumar L A <mohankumarl@hcl.com>
2019-02-26 18:59:52 +0530

    added Unit Test Cases

--- commerce-admin-console/src/app/features/catalogs/components/unassigned-cat-entries/unassigned-cat-entries.component.spec.ts
+++ commerce-admin-console/src/app/features/catalogs/components/unassigned-cat-entries/unassigned-cat-entries.component.spec.ts
@@ -1,14 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UnassignedCatEntriesComponent } from './unassigned-cat-entries.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateLoader, TranslateModule, TranslateFakeLoader, TranslateService } from '@ngx-translate/core';
 
-describe('UnassignedCatEntriesComponent', () => {
+fdescribe('UnassignedCatEntriesComponent', () => {
   let component: UnassignedCatEntriesComponent;
   let fixture: ComponentFixture<UnassignedCatEntriesComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UnassignedCatEntriesComponent ]
+      imports: [RouterTestingModule, TranslateModule.forRoot()],
+      declarations: [ UnassignedCatEntriesComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -21,5 +25,11 @@ describe('UnassignedCatEntriesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain "Translation header!"', () => {
+    const headerElement: HTMLElement = fixture.nativeElement;
+    const header  = headerElement.querySelector('h3');
+    expect(header.textContent).toContain('CATALOGS.UNASSIGNED_CATALOG_ENTRIES.CATALOG_ENTRIES_LIST');
   });
 });
