a39bdf6af441ce61d1c37a95311a6ff63ef2449b
Asutosh Roy <asutoroy@in.ibm.com>
2019-02-26 19:10:44 +0530

    unit test cases newMasterCategory component

--- commerce-admin-console/src/app/features/catalogs/components/new-master-category/new-master-category.component.spec.ts
+++ commerce-admin-console/src/app/features/catalogs/components/new-master-category/new-master-category.component.spec.ts
@@ -1,13 +1,18 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router, Routes } from '@angular/router';
+import { Location } from '@angular/common';
 import { NewMasterCategoryComponent } from './new-master-category.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { FormsModule } from '@angular/forms';
 import { DialogModule } from 'carbon-components-angular';
+
 fdescribe('NewMasterCategoryComponent', () => {
   let component: NewMasterCategoryComponent;
   let fixture: ComponentFixture<NewMasterCategoryComponent>;
+  let router: Router;
+  let location: Location;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,6 +24,9 @@ fdescribe('NewMasterCategoryComponent', () => {
   }));
 
   beforeEach(() => {
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
+    router.initialNavigation();
     fixture = TestBed.createComponent(NewMasterCategoryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -26,5 +34,31 @@ fdescribe('NewMasterCategoryComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should check save clicked', () => {
+    component.saveMasterData();
+    expect(component).toBeTruthy();
+  });
+  it('navigate to "extendedSite" takes you to /catalogs/extendedSite', fakeAsync(() => {
+    router.navigate(['/catalogs/extendedSite']).then(() => {
+      expect(location.path()).toBe('/catalogs/extendedSite');
+    });
+  }));
+  it('navigate to "catalogs" takes you to /catalogs', fakeAsync(() => {
+    router.navigate(['/catalogs']).then(() => {
+      expect(location.path()).toBe('/catalogs');
+    });
+  }));
+  it('should contain "Translation header!"', () => {
+    const headerElement: HTMLElement = fixture.nativeElement;
+    expect(headerElement.textContent).toContain('CATALOGS.NEW_MASTER_CATEGORY.header_text');
+  });
+  it('should contain "AssociatedStore!"', () => {
+    const headerElement: HTMLElement = fixture.nativeElement;
+    expect(headerElement.textContent).toContain('CATALOGS.NEW_MASTER_CATEGORY.associated_store');
+  });
+  it('should contain "AuroraESite!"', () => {
+    const headerElement: HTMLElement = fixture.nativeElement;
+    expect(headerElement.textContent).toContain('CATALOGS.NEW_MASTER_CATEGORY.auroraESite');
   });
 });
