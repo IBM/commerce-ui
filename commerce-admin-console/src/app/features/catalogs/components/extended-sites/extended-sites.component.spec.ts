2bdf12eabfbb6d472740c3e3f4f815b41e6bfa4f
Mohan kumar L A <mohankumarl@hcl.com>
2019-02-26 18:59:52 +0530

    added Unit Test Cases

--- commerce-admin-console/src/app/features/catalogs/components/extended-sites/extended-sites.component.spec.ts
+++ commerce-admin-console/src/app/features/catalogs/components/extended-sites/extended-sites.component.spec.ts
@@ -1,23 +1,35 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ExtendedSitesComponent } from './extended-sites.component';
+import { Routes, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { TranslateModule } from '@ngx-translate/core';
+import { Location} from "@angular/common";
+import { TranslateLoader, TranslateModule, TranslateFakeLoader, TranslateService } from '@ngx-translate/core';
+import { MasterCategoryService } from '../../services/masterCategory/master-category.service';
+import { NewMasterCategoryComponent } from '../new-master-category/new-master-category.component';
+
 fdescribe('ExtendedSitesComponent', () => {
   let component: ExtendedSitesComponent;
   let fixture: ComponentFixture<ExtendedSitesComponent>;
+  let router: Router;
+  let location: Location;
+  let extCategoryService: MasterCategoryService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule, TranslateModule.forRoot()],
       declarations: [ ExtendedSitesComponent ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [MasterCategoryService],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    router = TestBed.get(Router); 
+    location = TestBed.get(Location);
     fixture = TestBed.createComponent(ExtendedSitesComponent);
+    router.initialNavigation();
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -25,4 +37,42 @@ fdescribe('ExtendedSitesComponent', () => {
   it('should create ', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain "Translation header!"', () => {
+    const headerElement: HTMLElement = fixture.nativeElement;
+    const header  = headerElement.querySelector('h3');
+    expect(header.textContent).toContain('CATALOGS.EXTENDED_SITES.header');
+  });
+
+  // it('should be able to navigate to /catalogs/masterCategory!!!!!!!!!!!!', (done) => {
+  //   // var org = null;
+  //   component.onSelect();
+  //   setTimeout(function() {  
+  //   expect(location.path()).toBe('/');
+  //   done();
+  //   }, 1000);
+  // });
+
+  it("fakeAsync works", fakeAsync(() => {
+    let promise = new Promise(resolve => {
+      setTimeout(resolve, 10);
+    });
+    let done = false;
+    promise.then(() => (done = true));
+    tick(50);
+    expect(done).toBeTruthy();
+  }));
+
+  it('navigate to "" redirects you to /home', fakeAsync(() => {
+    router.navigate([""]).then(() => {
+      expect(location.path()).toBe("/");
+    });
+  }));
+
+  it('navigate to "search" takes you to /masterCategory', fakeAsync(() => {
+    component.onSelect();
+    router.navigate(["/catalogs/masterCategory"]).then(() => {
+      expect(location.path()).toBe("/catalogs/masterCategory");
+    });
+  }));
 });
