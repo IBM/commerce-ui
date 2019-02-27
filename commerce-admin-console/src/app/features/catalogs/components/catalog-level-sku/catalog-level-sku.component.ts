703000a3fe4910fede43ffe75849e8e26835d714
charan kumar <charankumar@charans-MacBook-Pro.local>
2019-02-27 16:18:09 +0530

    fixed issue with routing for some components

---
+++ commerce-admin-console/src/app/features/catalogs/components/catalog-level-sku/catalog-level-sku.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-level-sku',
+  templateUrl: './catalog-level-sku.component.html',
+  styleUrls: ['./catalog-level-sku.component.scss']
+})
+export class CatalogLevelSkuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
