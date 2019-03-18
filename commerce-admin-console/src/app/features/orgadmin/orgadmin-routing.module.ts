7483c5c714803c4936e8b6998200b3b3c13a7312
Asutosh Roy <asutoroy@in.ibm.com>
2019-03-18 18:29:20 +0530

    added account nfo

--- commerce-admin-console/src/app/features/orgadmin/orgadmin-routing.module.ts
+++ commerce-admin-console/src/app/features/orgadmin/orgadmin-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './components/users/users/users.component';
+import { AccountInfoComponent } from './components/users/account-info/account-info.component';
  
 export const routes: Routes = [
     {
-        path: '', component: UsersComponent
+        path: '', component: AccountInfoComponent
     }
 ];
 
--- commerce-admin-console/src/app/features/orgadmin/orgadmin.module.ts
+++ commerce-admin-console/src/app/features/orgadmin/orgadmin.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrgadminRoutingModule} from './orgadmin-routing.module';
-import { UsersComponent } from './components/users/users/users.component'
+import { IconModule } from '@carbon/icons-angular';
+import { ButtonModule,
+  TabsModule, AccordionModule, CheckboxModule, RadioModule, TableModule, DropdownModule,
+LinkModule, ModalModule, SearchModule, PlaceholderModule, DialogModule, UIShellModule, FileUploaderModule, NotificationModule  } from 'carbon-components-angular';
+import { UsersComponent } from './components/users/users/users.component';
+import { AccountInfoComponent } from './components/users/account-info/account-info.component'
 @NgModule({
   imports: [
     CommonModule,
-    OrgadminRoutingModule
+    OrgadminRoutingModule,
+    IconModule,
+    TabsModule,
+    AccordionModule, 
+    CheckboxModule, 
+    RadioModule,
+    TableModule, 
+    DropdownModule,
+    LinkModule,
+    ModalModule,
+    SearchModule,
+    PlaceholderModule,
+    DialogModule,
+    UIShellModule,
+    FileUploaderModule,
+    NotificationModule
   ],
-  declarations: [UsersComponent]
+  declarations: [UsersComponent, AccountInfoComponent]
 })
 export class OrgadminModule { }
