530b83502791e99f4c7768e0e5939547c67a0ed5
Mohan kumar L A <mohankumarl@hcl.com>
2019-03-22 19:21:08 +0530

    integrating orgAdmin with userManagement

---
+++ commerce-admin-console/src/app/features/user-management/components/account-info/account-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ac-account-info',
+  templateUrl: './account-info.component.html',
+  styleUrls: ['./account-info.component.scss']
+})
+export class AccountInfoComponent implements OnInit {
+  showInput: boolean;
+  constructor() { }
+
+  ngOnInit() {
+    this.showInput = true;
+  }
+}
