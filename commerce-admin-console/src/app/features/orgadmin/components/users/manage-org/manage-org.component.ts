8f33a7f8da8036bf0fdc37c657bebbecc9af48e5
Mohan kumar L A <mohankumarl@hcl.com>
2019-03-22 12:14:10 +0530

    removed box-grid

--- commerce-admin-console/src/app/features/orgadmin/components/users/manage-org/manage-org.component.ts
+++ commerce-admin-console/src/app/features/orgadmin/components/users/manage-org/manage-org.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-manage-org',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageOrgComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
