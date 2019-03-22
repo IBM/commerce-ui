f3427253b3881b256080860ed8a924140a4fb83c
Mohan kumar L A <mohankumarl@hcl.com>
2019-03-22 19:21:08 +0530

    integrating orgAdmin with userManagement

--- commerce-admin-console/src/app/features/user-management/user-management.module.ts
+++ commerce-admin-console/src/app/features/user-management/user-management.module.ts
@@ -7,32 +7,19 @@ import { ComponentsModule } from '../../components/components.module';
 import { UserManagementRoutingModule } from './user-management-routing.module';
 import {
   NFormsModule, DropdownModule, TableModule, PaginationModule,
-  DialogModule, ModalModule, TabsModule
+  DialogModule, ModalModule
 } from 'carbon-components-angular';
-import { Search16Module } from '@carbon/icons-angular/lib/search/16';
-import { Add16Module } from '@carbon/icons-angular/lib/add/16';
-import { ChevronRight16Module } from '@carbon/icons-angular/lib/chevron--right/16';
-import { Menu32Module } from '@carbon/icons-angular/lib/menu/32';
-import { CheckmarkOutline16Module } from '@carbon/icons-angular/lib/checkmark--outline/16';
-import { ArrowDown16Module } from '@carbon/icons-angular/lib/arrow--down/16';
-import { Close16Module } from '@carbon/icons-angular/lib/close/16';
-import { CheckmarkFilled16Module } from '@carbon/icons-angular/lib/checkmark--filled/16';
-import { View16Module } from '@carbon/icons-angular/lib/view/16';
-import { ViewOff16Module } from '@carbon/icons-angular/lib/view--off/16';
-import { Settings16Module } from '@carbon/icons-angular/lib/settings/16';
-import { Filter16Module } from '@carbon/icons-angular/lib/filter/16';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { IconModule } from '@carbon/icons-angular';
+
+import { SearchModalComponent } from './user-list/search-modal/search-modal.component';
+import { AddUserComponent } from './add-user/add-user.component';
 import { TranslateModule } from '@ngx-translate/core';
-import { UserGroupsComponent } from './components/users/user-groups/user-groups.component';
-import { UserRolesComponent } from './components/users/user-roles/user-roles.component';
-import { UserAccountComponent } from './components/users/user-account/user-account.component';
-import { UserContactComponent } from './components/users/user-contact/user-contact.component';
-import { ManageUserAccountComponent } from './components/users/manage-user-account/manage-user-account.component';
-import { ManageUserContactComponent } from './components/users/manage-user-contact/manage-user-contact.component';
-import { ManageUserGroupsComponent } from './components/users/manage-user-groups/manage-user-groups.component';
-import { ManageUserRolesComponent } from './components/users/manage-user-roles/manage-user-roles.component';
-import { UserListComponent } from './components/users/user-list/user-list.component';
-import { OrganizationListComponent } from './components/organizations/organization-list/organization-list.component';
+import { ButtonBarComponent } from './user-list/button-bar/button-bar.component';
+import { UserListDashboardComponent } from './user-list/user-list-dashboard/user-list-dashboard.component';
+import { UserTableComponent } from './user-list/user-table/user-table.component';
+import { ManageUserComponent } from './components/manage-user/manage-user.component';
+import { AccountInfoComponent } from './components/account-info/account-info.component';
+import { OrganisationLandingPageComponent } from './components/organisation-landing-page/organisation-landing-page.component';
 
 @NgModule({
   imports: [
@@ -66,16 +53,14 @@ import { OrganizationListComponent } from './components/organizations/organizati
     Filter16Module
   ],
   declarations: [
-    UserGroupsComponent,
-    UserRolesComponent,
-    UserAccountComponent,
-    UserContactComponent,
-    ManageUserAccountComponent,
-    ManageUserContactComponent,
-    ManageUserGroupsComponent,
-    ManageUserRolesComponent,
-    UserListComponent,
-    OrganizationListComponent
+    SearchModalComponent,
+    AddUserComponent,
+    ButtonBarComponent,
+    UserListDashboardComponent,
+    UserTableComponent,
+    ManageUserComponent,
+    AccountInfoComponent,
+    OrganisationLandingPageComponent
   ],
   providers: [
   ],
