f3afeb080d7e4585b5a0ba216d4eb3cdafc773ae
Mohan kumar L A <mohankumarl@hcl.com>
2019-03-25 17:44:54 +0530

    integrated components into usermanagement

--- commerce-admin-console/src/app/features/user-management/user-management-routing.module.ts
+++ commerce-admin-console/src/app/features/user-management/user-management-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddUserComponent } from './add-user/add-user.component';
 import { UserListDashboardComponent } from './user-list/user-list-dashboard/user-list-dashboard.component';
+import { UsersComponent } from './components/users/users.component';
 import { ManageUserComponent } from './components/manage-user/manage-user.component';
 import { AccountInfoComponent } from './components/account-info/account-info.component';
 import { OrganisationLandingPageComponent } from './components/organisation-landing-page/organisation-landing-page.component';
+import { ContactInfoComponent } from './components/contact-info/contact-info.component';
+import { UserRolesComponent } from './components/user-roles/user-roles.component';
 
 const routes: Routes = [
   {
-    path: '', component: ManageUserComponent
+    path: '', component: UsersComponent
   },
   {
     path: 'add-user', component: AddUserComponent
@@ -24,6 +27,15 @@ const routes: Routes = [
   },
   {
     path: 'landing_page', component: OrganisationLandingPageComponent
+  },
+  {
+      path: 'contactIngo', component: ContactInfoComponent
+  },
+  {
+      path: 'userRoles', component: UserRolesComponent
+  },
+  {
+      path: 'users', component: UsersComponent
   }
 ];
 
