f016e003a2d5b2191240dfcdb49cec81f6c48ca1
Mohan kumar L A <mohankumarl@hcl.com>
2019-03-28 18:07:20 +0530

    css fix

--- commerce-admin-console/src/app/features/user-management/components/users/manage-user-roles/manage-user-roles.component.ts
+++ commerce-admin-console/src/app/features/user-management/components/users/manage-user-roles/manage-user-roles.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-manage-user-roles',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageUserRolesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  saveRoles() {
+    this.router.navigate(['/users/manageGroups']);
   }
 
 }
