6e765e08ca719058189cf9bd9836fdd8037be8d9
Sriman Soumyadarsan Mishra <sriman@Srimans-MacBook-Pro.local>
2019-03-28 13:25:38 +0530

    Functionality for adding the roles.

--- commerce-admin-console/src/app/features/user-management/components/users/user-roles/user-roles.component.ts
+++ commerce-admin-console/src/app/features/user-management/components/users/user-roles/user-roles.component.ts
@@ -9,6 +9,9 @@ export class UserRolesComponent implements OnInit {
 
   model: '';
   disabled: '';
+  rolesArray:any=[];
+  result:any=[];
+  modelRoles:'';
   constructor() { }
 
   orgList = [
@@ -24,15 +27,58 @@ export class UserRolesComponent implements OnInit {
   rolesList = [
     {
       content: 'Developer',
-      selected: false
+      selected: true
     },
     {
       content: 'Tester',
+      selected: false,
+    },
+    {
+      content: 'Architect',
+      selected: false,
+    },
+    {
+      content: 'Test Lead',
       selected: false,
     }
   ];
 
   ngOnInit() {
   }
+  mySelectedRoles(mySelectedRoles:any){ 
 
+    
+    
+    if(mySelectedRoles != null){
+      this.rolesArray.push(mySelectedRoles) ;
+      this.removeDuplicate(this.rolesArray);
+    }
+  }
+  removeDuplicate(rolesArray:any){
+    let index;
+    let len = rolesArray.length;
+    let resultArray=[];
+    let obj = {}; 
+    for (index = 0; index<len; index++)  {            
+      obj[rolesArray[index]] = 0;        
+   }  
+   for (index in obj) {            
+    resultArray.push(index);        
+   }  
+   this.result = resultArray;    
+} 
+RemoveRoles(removeRoles:any){
+for( var i = 0; i < this.result.length; i++){
+  if ( this.result[i] === removeRoles) {
+    this.result.splice(i, 1); 
+    this.rolesArray.splice(i, 1);
+    this.modelRoles = null;
+    if(this.result.length===0){
+      this.result=[];
+      this.rolesArray = [];
+    }
+  }
+}
+
+}
 }
