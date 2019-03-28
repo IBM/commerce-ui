7f847aa8f9de7ac8e8a580aef3bf79365dfa2c1e
Naresh  Reddy Gadi <naresh@Nareshs-MacBook-Pro.local>
2019-03-28 16:40:58 +0530

    Added Manage Groups and OrgList

--- commerce-admin-console/src/app/features/user-management/components/users/manage-user-groups/manage-user-groups.component.ts
+++ commerce-admin-console/src/app/features/user-management/components/users/manage-user-groups/manage-user-groups.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-user-groups.component.scss']
 })
 export class ManageUserGroupsComponent implements OnInit {
+  
+  groupListData: any;
 
+  groupList = [
+    { id: 1, groupName: 'AccountAdministatorsRole1' },
+    { id: 2, groupName: 'AccountAdministatorsRole2' },
+    { id: 3, groupName: 'AccountAdministatorsRole3' },
+    { id: 4, groupName: 'AccountAdministatorsRole4' },
+    { id: 5, groupName: 'AccountAdministatorsRole5' },
+    { id: 6, groupName: 'AccountAdministatorsRole6' },
+    { id: 7, groupName: 'AccountAdministatorsRole7' },
+    { id: 8, groupName: 'AccountAdministatorsRole8' },
+    { id: 9, groupName: 'AccountAdministatorsRole9' },
+    { id: 10, groupName: 'AccountAdministatorsRole10' }
+  ];
   constructor() { }
 
   ngOnInit() {
+    this.groupListData = this.groupList;
+    console.log(this.groupListData);
   }
-
 }
