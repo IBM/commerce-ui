2ad12ac090c059daad0897ce850c4899a75d4051
Sriman Soumyadarsan Mishra <sriman@Srimans-MacBook-Pro.local>
2019-04-09 17:37:24 +0530

    added organizations components

--- commerce-admin-console/src/app/features/user-management/components/users/user-list/user-list.component.ts
+++ commerce-admin-console/src/app/features/user-management/components/users/user-list/user-list.component.ts
@@ -64,15 +64,31 @@ export class UserListComponent implements OnInit {
     const fullPage = [];
 
     for (
-      let i = (page - 1) * this.model.pageLength;
-      i < page * this.model.pageLength && i < this.model.totalDataLength;
-      i++
+     let i = (page - 1) * this.model.pageLength;
+     i < page * this.model.pageLength && i < this.model.totalDataLength;
+     i++
     ) {
-      fullPage.push(line(i + 1));
+     fullPage.push(line(i + 1));
     }
 
     return new Promise(resolve => {
       setTimeout(() => resolve(fullPage), 150);
+      this.usersService.UsersGetManageableUsers({
+        offset:(page-1)*this.model.pageLength,
+        limit:this.model.pageLength
+      }).subscribe((body: any) => {
+        this.model.totalDataLength = body.count;
+        let data = [];
+        for (let i = 0; i < body.items.length; i++) {
+          let item = body.items[i];
+          let logonId = item.logonId;
+          let firstName = item.address ? item.address.firstName : '';
+          let lastName = item.address ? item.address.lastName : '';
+          let organizationName = item.organizationName;
+          data.push([logonId, firstName, lastName, organizationName, '', '']);
+        }
+        resolve(data);
+      });
     });
   }
 
