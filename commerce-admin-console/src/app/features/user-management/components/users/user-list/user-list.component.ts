c71c38d10ed5a45f921210a5cd18a5556aefba0c
Sriman Soumyadarsan Mishra <sriman@Srimans-MacBook-Pro.local>
2019-04-11 18:22:56 +0530

    User list API integration

--- commerce-admin-console/src/app/features/user-management/components/users/user-list/user-list.component.ts
+++ commerce-admin-console/src/app/features/user-management/components/users/user-list/user-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Input, ViewChild, TemplateRef } from '@angular/core'
 import { TableModel, TableHeaderItem, TableItem } from 'carbon-components-angular';
 import { Router } from '@angular/router';
 import { UsersService } from '../../../../../rest/services/users.service';
+import { UserMainService } from '../../../services/user-main.service';
+import { TranslateService } from '@ngx-translate/core';
+import { IframeService } from '../../../../../services/iframe.service';
 
 @Component({
   selector: 'ac-user-list',
@@ -10,25 +13,40 @@ import { UsersService } from '../../../../../rest/services/users.service';
 })
 export class UserListComponent implements OnInit {
 
-  @Input() striped: boolean = false;
-  
+  loginArray: any = [];
+  userListData: any;
+  @Input() striped = false;
+  userResult: any;
+  //specificUserId: string
+  id: string;
+
   @Input() model = new TableModel();
-  
+
   @Input() get totalDataLength() {
     return this.model.totalDataLength;
   }
   set totalDataLength(value) {
     this.model.totalDataLength = value;
   }
-  
+
   @ViewChild('paginationTableItemTemplate')
   protected paginationTableItemTemplate: TemplateRef<any>;
-  
-  constructor(private router: Router, private usersService: UsersService) { }
+  @ViewChild('paginationTableItemTemplate')
+
+  @ViewChild('listUserItemTemplate')
+  protected listUserItemTemplate: TemplateRef<any>;
+
+
+
+
+  constructor(private router: Router, private usersService: UsersService,
+    private userMainservice: UserMainService,
+    private translateService: TranslateService,
+    private iframeService: IframeService) { }
 
   ngOnInit() {
-    
-  
+
+
     this.model.data = [[]];
     this.model.header = [
       new TableHeaderItem({ data: 'Login ID' }),
@@ -38,51 +56,81 @@ export class UserListComponent implements OnInit {
       new TableHeaderItem({ data: 'Role' }),
       new TableHeaderItem({ })
     ];
+
+    // this.getUsersList();
     this.model.pageLength = 10;
-    this.model.totalDataLength = 15;
-    
-    
-    this.selectPage(1);
+    // this.model.totalDataLength = 15;
+
+
+     this.selectPage(1);
   }
-  customSort(index: number) {
-    this.sort(this.model, index);
-  }
-  sort(Tablemodel, index: number) {
-    if (Tablemodel.header[index].sorted) {
-      // if already sorted flip sorting direction
-      Tablemodel.header[index].ascending = Tablemodel.header[index].descending;
-    }
-    Tablemodel.sort(index);
-  }
+  // getUsersList() {
+  //   this.userMainservice.userList().then(results => {
+  //     this.userResult = Object.assign([], results);
+  //     if (this.userResult.items.length > 0) {
+  //       this.model.totalDataLength = this.userResult.length;
+  //       for (const org of this.userResult.items) {
+  //         this.model.addRow([
+  //           new TableItem({ data: { name: org['logonId'], link: '/users' + org['logonId'] }, template: this.listUserItemTemplate }),
+  //           new TableItem({ data: org['id'] }),
+  //           new TableItem({ data: org['receiveSmsNotification'] }),
+  //           new TableItem({ data: org['organizationId'] }),
+  //           new TableItem({ data: org['registrationType'] })
+  //         ]);
+  //       }
+  //     }
+
+  //   }).catch(() => {
+  //     this.translateService
+  //       .get('CATALOGS.HEADR.store_list_failed')
+  //       .subscribe((msg: string) => {
+  //         this.iframeService.postStatusMsg(msg, 'error');
+  //       });
+  //   });
+  // }
+
+  // sort
+  // customSort(index: number) {
+  //   this.sort(this.model, index);
+  // }
+  // sort(Tablemodel, index: number) {
+  //   if (Tablemodel.header[index].sorted) {
+  //     // if already sorted flip sorting direction
+  //     Tablemodel.header[index].ascending = Tablemodel.header[index].descending;
+  //   }
+  //   Tablemodel.sort(index);
+  // }
 
   getPage(page: number) {
-    const line = line => [`dave_evans`, `Dave`,`Evans`,`Organization A`,`Site Administrator`,``];
+    // const line = line => [`dave_evans`, `Dave`, `Evans`, `Organization A`, `Site Administrator`, ``];
 
-    const fullPage = [];
+    // const fullPage = [];
 
-    for (
-     let i = (page - 1) * this.model.pageLength;
-     i < page * this.model.pageLength && i < this.model.totalDataLength;
-     i++
-    ) {
-     fullPage.push(line(i + 1));
-    }
+    // for (
+    //  let i = (page - 1) * this.model.pageLength;
+    //  i < page * this.model.pageLength && i < this.model.totalDataLength;
+    //  i++
+    // ) {
+    //  fullPage.push(line(i + 1));
+    // }
 
     return new Promise(resolve => {
-      setTimeout(() => resolve(fullPage), 150);
+      //setTimeout(() => resolve(fullPage), 150);
       this.usersService.UsersGetManageableUsers({
-        offset:(page-1)*this.model.pageLength,
-        limit:this.model.pageLength
+        offset: (page - 1) * this.model.pageLength,
+        limit: this.model.pageLength
       }).subscribe((body: any) => {
+       this.userListData = body.items;
         this.model.totalDataLength = body.count;
-        let data = [];
+        const data = [];
         for (let i = 0; i < body.items.length; i++) {
-          let item = body.items[i];
-          let logonId = item.logonId;
-          let firstName = item.address ? item.address.firstName : '';
-          let lastName = item.address ? item.address.lastName : '';
-          let organizationName = item.organizationName;
-          data.push([logonId, firstName, lastName, organizationName, '', '']);
+          const item = body.items[i];
+          const logonId = item.logonId;
+          const firstName = item.address ? item.address.firstName : '';
+          const lastName = item.address ? item.address.lastName : '';
+          const organizationName = item.organizationName;
+          data.push([ logonId, firstName, lastName, organizationName, '', '']);
+          this.loginArray.push([logonId]);
         }
         resolve(data);
       });
@@ -90,35 +138,56 @@ export class UserListComponent implements OnInit {
   }
 
   selectPage(page) {
-    
+
     this.getPage(page).then((data: Array<Array<any>>) => {
       // set the data and update page
-      this.model.data = this.prepareData(data);
+      this.model.data = this.prepareData(data, this.loginArray);
       this.model.currentPage = page;
     });
   }
 
-  protected prepareData(data: Array<Array<any>>) {
+  protected prepareData(data: Array<Array<any>>, loginArray: Array<Array<any>>) {
     // create new data from the service data
-    let newData = [];
+    const newData = [];
+    // loginArray.forEach(dataRow => {
+    //   const row1 = [] ;
+    //   dataRow.forEach(dataElement => {
+    //     row1.push(new TableItem({
+    //       data: dataElement
+    //     }));
+    //         });
+    //         newData.push(row1);
+    // });
     data.forEach(dataRow => {
-      let row = [];
+      const row = [];
+
       dataRow.forEach(dataElement => {
         row.push(
           new TableItem({
-            data: dataElement,
-            template: typeof dataElement === 'string' ? undefined : this.paginationTableItemTemplate,
-            // your template can handle all the data types so you don't have to conditionally set it
-            // you can also set different templates for different columns based on index
-          })
-        );
+            data: { name: dataElement  }, template: this.listUserItemTemplate
+          }));
       });
-      newData.push(row);
-    });
+        newData.push(row);
+      });
+
     return newData;
   }
+
   createUser() {
     this.router.navigate(['users/userAccount']);
   }
+  userId(logonId: any) {
+    if (this.userListData) {
+      debugger;
+      //console.log('itm', this.userListData);
+      //console.log('list', this.userListData);
+      this.userListData.forEach(function(value) {
+        //console.log('val', value);
+        // if (value.logonId === logonId) {
+        //    this.id = value.id;
+        // }
+      });
+    }
 
+  }
 }
