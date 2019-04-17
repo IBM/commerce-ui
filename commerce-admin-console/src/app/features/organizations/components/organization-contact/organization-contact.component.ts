32e0cd194385dc1db477f921b104dac86e43d5ab
Sriman Soumyadarsan Mishra <sriman@Srimans-MacBook-Pro.local>
2019-04-17 16:19:41 +0530

    Organization Navigations are added.

--- commerce-admin-console/src/app/features/organizations/components/organization-contact/organization-contact.component.ts
+++ commerce-admin-console/src/app/features/organizations/components/organization-contact/organization-contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-organization-contact',
   templateUrl: './organization-contact.component.html',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizationContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  routeOrganizationDetails() {
+    this.router.navigate(['organizations/organizationsDetails']);
+  }
+  routeOrganizationRoles() {
+    this.router.navigate(['organizations/organizationsRoles']);
+  }
 }
--- commerce-admin-console/src/app/features/organizations/components/organization-roles/organization-roles.component.spec.ts
+++ commerce-admin-console/src/app/features/organizations/components/organization-roles/organization-roles.component.spec.ts
@@ -1,13 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { OrganizationRolesComponent } from './organization-roles.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { DialogModule } from 'carbon-components-angular';
+import { TranslateModule } from '@ngx-translate/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Search16Module } from '@carbon/icons-angular/lib/search/16';
+import { Add16Module } from '@carbon/icons-angular/lib/add/16';
+import { ChevronRight16Module } from '@carbon/icons-angular/lib/chevron--right/16';
+import { Menu32Module } from '@carbon/icons-angular/lib/menu/32';
+import { CheckmarkOutline16Module } from '@carbon/icons-angular/lib/checkmark--outline/16';
+import { ArrowDown16Module } from '@carbon/icons-angular/lib/arrow--down/16';
+import { CheckmarkFilled16Module } from '@carbon/icons-angular/lib/checkmark--filled/16';
+import { View16Module } from '@carbon/icons-angular/lib/view/16';
+import { ViewOff16Module } from '@carbon/icons-angular/lib/view--off/16';
+import { HttpClientModule } from '@angular/common/http';
 
-describe('OrganizationRolesComponent', () => {
+fdescribe('OrganizationRolesComponent', () => {
   let component: OrganizationRolesComponent;
   let fixture: ComponentFixture<OrganizationRolesComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientModule, FormsModule, DialogModule, Search16Module, Add16Module, ChevronRight16Module,
+        Menu32Module, CheckmarkOutline16Module, ArrowDown16Module, CheckmarkFilled16Module, View16Module, ViewOff16Module,
+        TranslateModule.forRoot()],
+     schemas: [CUSTOM_ELEMENTS_SCHEMA],
       declarations: [ OrganizationRolesComponent ]
     })
     .compileComponents();
@@ -22,4 +40,7 @@ describe('OrganizationRolesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+
 });
