6b1a6c3e2990bf1e42477c84707cdad78acfa2e3
Naresh  Reddy Gadi <naresh@Nareshs-MacBook-Pro.local>
2019-04-17 16:37:39 +0530

    Organization list fixed

--- commerce-admin-console/src/app/features/organizations/components/organization-list/organization-list.component.spec.ts
+++ commerce-admin-console/src/app/features/organizations/components/organization-list/organization-list.component.spec.ts
@@ -1,11 +1,42 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Routes, Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Location} from "@angular/common";
+import { TranslateLoader, TranslateModule, TranslateFakeLoader, TranslateService } from '@ngx-translate/core';
 
 import { OrganizationListComponent } from './organization-list.component';
 
-describe('OrganizationListComponent', () => {
+// describe('OrganizationListComponent', () => {
+//   let component: OrganizationListComponent;
+//   let fixture: ComponentFixture<OrganizationListComponent>;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ OrganizationListComponent ]
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(OrganizationListComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
+
+
+
+
+fdescribe('OrganisationLandingPageComponent', () => {
   let component: OrganizationListComponent;
   let fixture: ComponentFixture<OrganizationListComponent>;
-
+  let router: Router;
+  let location: Location;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ OrganizationListComponent ]
@@ -13,8 +44,22 @@ describe('OrganizationListComponent', () => {
     .compileComponents();
   }));
 
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, TranslateModule.forRoot()],
+      declarations: [ OrganizationListComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [],
+    })
+    .compileComponents();
+  }));
+
   beforeEach(() => {
+    router = TestBed.get(Router); 
+    location = TestBed.get(Location);
     fixture = TestBed.createComponent(OrganizationListComponent);
+    router.initialNavigation();
+
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -22,4 +67,37 @@ describe('OrganizationListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('Organisation landing should contain "Translation header!"', () => {
+    const headerElement: HTMLElement = fixture.nativeElement;
+    const header  = headerElement.querySelector('h3');
+    expect(header.textContent).toContain('organizations');
+  });
+  
+  it("fakeAsync works", fakeAsync(() => {
+    let promise = new Promise(resolve => {
+      setTimeout(resolve, 10);
+    });
+    let done = false;
+    promise.then(() => (done = true));
+    tick(50);
+    expect(done).toBeTruthy();
+  }));
+
+  it('navigate to "" redirects you to /home', fakeAsync(() => {
+    router.navigate([""]).then(() => {
+      expect(location.path()).toBe("/");
+    });
+  }));
+
+  // it('navigate to "search" takes you to /masterCategory', fakeAsync(() => {
+  //   component.onSelect();
+  //   router.navigate(["/catalogs/masterCategory"]).then(() => {
+  //     expect(location.path()).toBe("/catalogs/masterCategory");
+  //   });
+  // }));
+
+
 });
+
--- commerce-admin-console/src/app/features/organizations/components/organization-list/organization-list.component.ts
+++ commerce-admin-console/src/app/features/organizations/components/organization-list/organization-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, TemplateRef, ViewChild,ViewEncapsulation,Input } from '@angular/core';
-import { TableModel, TableItem, TableHeaderItem  } from 'carbon-components-angular';
+import { Component, OnInit, TemplateRef, ViewChild, ViewEncapsulation, Input } from '@angular/core';
+import { TableModel, TableItem, TableHeaderItem } from 'carbon-components-angular';
 import { TranslateService } from '@ngx-translate/core';
 import { ActivatedRoute, Router } from '@angular/router';
 //import { OrganizationsConstants } from '../organizations.constants';
@@ -18,118 +18,132 @@ import { OrganizationsService } from '../../../../rest/services/organizations.se
 })
 
 export class OrganizationListComponent implements OnInit {
-@Input() striped = false;
+  organizationResponse: any;
+  @Input() striped = false;
 
-@Input() model = new TableModel();
+  @Input() model = new TableModel();
 
-@Input() get totalDataLength() {
-  return this.model.totalDataLength;
-}
-set totalDataLength(value) {
-  this.model.totalDataLength = value;
-}
-
-@ViewChild('paginationTableItemTemplate')
-protected paginationTableItemTemplate: TemplateRef<any>;
-@ViewChild('listOrgItemTemplate')
-protected listOrgItemTemplate: TemplateRef<any>;
-
-constructor(private router: Router, private orgService: OrganizationsService) { }
-
-ngOnInit() {
-  
-
-  
-  this.model.header = [
-    new TableHeaderItem({ data: 'Name'}),
-    new TableHeaderItem({ data: 'Parent Organization'}),
-    new TableHeaderItem({ data: 'Type' }),
-  ];
-  this.model.data = [[]];
-  this.model.pageLength = 10;
-  this.model.totalDataLength = 14;
-  
-  
-  this.selectPage(1);
-}
-customSort(index: number) {
-  this.sort(this.model, index);
-}
-sort(Tablemodel, index: number) {
-  if (Tablemodel.header[index].sorted) {
-    // if already sorted flip sorting direction
-    Tablemodel.header[index].ascending = Tablemodel.header[index].descending;
+  @Input() get totalDataLength() {
+    return this.model.totalDataLength;
   }
-  Tablemodel.sort(index);
-}
+  set totalDataLength(value) {
+    this.model.totalDataLength = value;
+  }
 
-getPage(page: number) {
-  //const line = line => [`dave_evans`, `Dave`,`Evans`,`Organization A`,`Site Administrator`,``];
+  @ViewChild('paginationTableItemTemplate')
+  protected paginationTableItemTemplate: TemplateRef<any>;
+  @ViewChild('listOrgItemTemplate')
+  protected listOrgItemTemplate: TemplateRef<any>;
 
-  //const fullPage = [];
+  constructor(private router: Router, private orgService: OrganizationsService) { }
 
-  //for (
-  //  let i = (page - 1) * this.model.pageLength;
-  //  i < page * this.model.pageLength && i < this.model.totalDataLength;
-  //  i++
-  //) {
-  //  fullPage.push(line(i + 1));
-  //}
+  ngOnInit() {
 
-  return new Promise(resolve => {
-    //setTimeout(() => resolve(fullPage), 150);
-    this.orgService.OrganizationGetManageableOrganizations({
-      offset:(page-1)*this.model.pageLength,
-      limit:this.model.pageLength
-    }).subscribe((body: any) => {
-      this.model.totalDataLength = body.count;
-      console.log("org" + this.totalDataLength);
-      let data = [];
-      for (let i = 0; i < body.items.length; i++) {
-        let item = body.items[i];
-        let name = item.name;
-        let parentOrganizationName = item.parentOrganizationName;
-        let organizationType = item.organizationType;
-        data.push([name, parentOrganizationName, organizationType]);
+
+
+    this.model.header = [
+      new TableHeaderItem({ data: 'Name' }),
+      new TableHeaderItem({ data: 'Parent Organization' }),
+      new TableHeaderItem({ data: 'Type' }),
+    ];
+    this.model.data = [[]];
+    this.model.pageLength = 10;
+    this.model.totalDataLength = this.organizationResponse;
+
+
+    this.selectPage(1);
+  }
+  customSort(index: number) {
+    this.sort(this.model, index);
+  }
+  sort(Tablemodel, index: number) {
+    if (Tablemodel.header[index].sorted) {
+      // if already sorted flip sorting direction
+      Tablemodel.header[index].ascending = Tablemodel.header[index].descending;
+    }
+    Tablemodel.sort(index);
+  }
+
+  getPage(page: number) {
+
+    return new Promise(resolve => {
+
+      this.orgService.OrganizationGetManageableOrganizations({
+        offset: (page - 1) * this.model.pageLength,
+        limit: this.model.pageLength
+      }).subscribe((body: any) => {
+        this.model.totalDataLength = body.count;
+        console.log("org" + this.totalDataLength);
+        const data = [];
+        for (let i = 0; i < body.items.length; i++) {
+          const item = body.items[i];
+          this.organizationResponse = item;
+          const name = item.name;
+          const parentOrganizationName = item.parentOrganizationName;
+          const organizationType = item.organizationType;
+          data.push([name, parentOrganizationName, organizationType]);
+        }
+        resolve(data);
+        console.log(data);
+        console.log(data[0]);
+      });
+    });
+  }
+
+  selectPage(page) {
+
+    this.getPage(page).then((data: Array<Array<any>>) => {
+      // set the data and update page
+      //console.log("page", data)
+      this.model.data = this.prepareData(data);
+      //   this.model.data = [
+      //     [new TableItem({data: {name:data[0][0], link: "/table"}, template: this.listOrgItemTemplate}), 
+      //     new TableItem({data:  data[0][1]}), new TableItem({data: data[2][2] })]
+      // ];
+      console.log("data[1][1]", data[0][1])
+      this.model.currentPage = page;
+    });
+  }
+
+  prepareData(data: Array<Array<any>>) {
+    const listData = [];
+    // for (let i = 0; i <= data.length-1; i++) {
+    //       listData.push( [new TableItem({data: {name: data[i][0]}, template: this.listOrgItemTemplate}), 
+    //       new TableItem({data:  data[i][1]}), new TableItem({data: data[i][2] })]);
+    // }
+    // return listData;
+    data.forEach((value, idx) => {
+      console.log('val', value);
+      console.log('idx', idx);
+      if (value[2] === 'O') {
+        listData.push([new TableItem({ data: { name: data[idx][0] }, template: this.listOrgItemTemplate }),
+        new TableItem({ data: data[idx][1] }), new TableItem({ data: 'Organization' })]);
+      } else {
+        listData.push([new TableItem({ data: { name: data[idx][0] }, template: this.listOrgItemTemplate }),
+        new TableItem({ data: data[idx][1] }), new TableItem({ data: 'Organization Unit' })]);
       }
-      resolve(data);
+
     });
-  });
-}
+    return listData;
+  }
 
-selectPage(page) {
-  
-  this.getPage(page).then((data: Array<Array<any>>) => {
-    // set the data and update page
-    this.model.data = this.prepareData(data);
-    this.model.currentPage = page;
-  });
-}
+  getSelectedOrganization(name) {
+    this.organizationResponse.forEach((value, idx) => {
+      if (value.name === name) {
+        this.manageOrgApiCall();
+      }
+    })
+    this.router.navigate(['organizations/organizationsDetails']);
+  }
+  manageOrgApiCall() {
 
-protected prepareData(data: Array<Array<any>>) {
-  // create new data from the service data
-  let newData = [];
-  data.forEach(dataRow => {
-    let row = [];
+  }
 
-    dataRow.forEach(dataElement => {
-      row.push(
-        new TableItem({
-          data: {name: dataElement}, template:  this.listOrgItemTemplate
-          //template: typeof dataElement === 'string' ? undefined : this.listOrgItemTemplate,
-        
-          // your template can handle all the data types so you don't have to conditionally set it
-          // you can also set different templates for different columns based on index
-        })
-      );
-    });
-    newData.push(row);
-  });
-  return newData;
-}
-routeOrganizationDetails() {
-  this.router.navigate(['organizations/organizationsDetails']);
-}
+
+
+  createOrganization() {
+    this.router.navigate(['organizations/organizationsDetails']);
+  }
 
 }
 
