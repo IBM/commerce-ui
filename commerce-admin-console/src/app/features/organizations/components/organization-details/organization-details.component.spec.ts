733d26b25731012b30d6c2c6acbd46bed6bfa0bf
Sriman Soumyadarsan Mishra <sriman@Srimans-MacBook-Pro.local>
2019-05-21 16:29:37 +0530

    Unit test for the manage user account and removing the error from the organization details

--- commerce-admin-console/src/app/features/organizations/components/organization-details/organization-details.component.spec.ts
+++ commerce-admin-console/src/app/features/organizations/components/organization-details/organization-details.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { OrganizationDetailsComponent, DetailsOrganization } from './organization-details.component';
+import { OrganizationDetailsComponent } from './organization-details.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Router, Routes } from '@angular/router';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
