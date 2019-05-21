8c709aeed52b070521b96ba23b805a5af11c6dc7
Sriman Soumyadarsan Mishra <sriman@Srimans-MacBook-Pro.local>
2019-05-21 16:29:37 +0530

    Unit test for the manage user account and removing the error from the organization details

--- commerce-admin-console/src/app/features/user-management/components/users/manage-user-account/manage-user-account.component.spec.ts
+++ commerce-admin-console/src/app/features/user-management/components/users/manage-user-account/manage-user-account.component.spec.ts
@@ -5,7 +5,7 @@ import { Router, Routes } from '@angular/router';
 import { Location } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, DebugElement, Injector } from '@angular/core';
 import { TranslateModule, TranslateLoader, TranslateFakeLoader, TranslateService } from '@ngx-translate/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule , ReactiveFormsModule} from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { DialogModule } from 'carbon-components-angular';
 import { Search16Module } from '@carbon/icons-angular/lib/search/16';
@@ -22,7 +22,10 @@ import { UsersService } from '../../../../../../../src/app/rest/services/users.s
 import { UserMainService } from '../../../services/user-main.service';
 import { IframeService } from '../../../../../services/iframe.service';
 import { Settings16Module } from '@carbon/icons-angular/lib/settings/16';
+import { SearchPipe } from '../../../pipe/search.pipe';
 
+import { UserAccountPolicyDescriptionsService } from '../../../../../rest/services/user-account-policy-descriptions.service';
+import { OrganizationsService } from '../../../../../rest/services/organizations.service';
 const routes: Routes = [
   {path: 'organizations', component: ManageUserAccountComponent}
 ];
@@ -42,11 +45,12 @@ fdescribe('ManageUserAccountComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ManageUserAccountComponent ],
+      declarations: [ ManageUserAccountComponent , SearchPipe],
       imports: [
         RouterTestingModule,
         HttpClientModule,
         FormsModule,
+        ReactiveFormsModule,
         DialogModule,
         Search16Module,
         Add16Module,
@@ -71,7 +75,9 @@ fdescribe('ManageUserAccountComponent', () => {
           UserMainService,
           IframeService,
           UserMainService,
-          MockManageUserAccountService
+          MockManageUserAccountService,
+          UserAccountPolicyDescriptionsService,
+          OrganizationsService
         ]
     })
     .compileComponents();
@@ -92,54 +98,93 @@ fdescribe('ManageUserAccountComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  it('should initialize successfully', () => {
-    var manageUserData = [];
-    var manageUserResponse = {
-      showInput: "boolean",
-      disabled: '',
-      accountData: "unitTest",
-      userAccountData: "unitTest",
-      logonId: "unitTest",
-      email1: "unitTest",
-      password: "unitTest",
-      passwordVerify: "unitTest",
-      organizationName: "unitTest",
-      policy: "unitTest",
-      emailPattern: "unitTest",
-      envalidEmail: "unitTest",
-      manageUserResponse: "unitTest",
-      id: "number"
-    }
-    manageUserData.push(manageUserResponse);
-    spyOn(usermainService, 'updateUser').and.returnValue(Promise.resolve(manageUserData));
-    component.ngOnInit();
-    setTimeout(function() {
-      expect(usermainService.updateUser).toHaveBeenCalled();
-      expect(component.updateUser.find(entry => entry.content == "unitTest")).toBeDefined;
-      // expect(component.parentOrgItems.find(entry => entry.content == "Root Organization")).toBeDefined;
-    }, 1000);
+  it('form invalid when empty', () => {
+    expect(component.accountForm.valid).toBeFalsy();
+  });
+  it('email field validity', () => {
+    const email1 = component.accountForm.controls['email1'];
+    expect(email1.valid).toBeFalsy();
+  });
+  it('email field validity, input required', () => {
+    let errors = {};
+    const email1 = component.accountForm.controls['email1'];
+    errors = email1.errors || {};
+    expect(errors['required']).toBeTruthy();
+  });
+  it('email field validity, pattern validation', () => {
+    let errors = {};
+    const email1 = component.accountForm.controls['email1'];
+    email1.setValue('test');
+errors = email1.errors || {};
+expect(errors['pattern']).toBeTruthy();
   });
 
-  it('the logonId should not be assigned given the logonId is not selected', () => {
-    // let selection : Object = {'item' : {'selected' : false, 'content' : ''}};
-    component.accountCall();
-    expect(component.logonId).toBeNull;
+  it('testing of the Account policy list service', () => {
+    fixture = TestBed.createComponent(ManageUserAccountComponent);
+    component = fixture.debugElement.componentInstance;
+    const accountPolicyService = fixture.debugElement.injector.get(UserAccountPolicyDescriptionsService);
+    fixture.detectChanges();
+    expect(accountPolicyService.getUserAccountPolicyDescriptions({}).subscribe(result => {
+      expect(result.items.length).toBeGreaterThan(0);
+    }));
   });
 
-  it('should find the correct existing user when the request is valid', () => {
-    // expect(component.validRequest()).toBe(false);
-    // let org : Object = {'item' : {'selected' : true, 'content' : 'unitTestOrg'}};
-    // let parentOrg : Object = {'item' : {'selected' : true, 'content' : 'parentUnitTestOrg'}};
-    // component.onOrgSelected(org);
-    // expect(component.validRequest()).toBe(true);
-    // component.onParentOrgSelected(parentOrg);
-    // expect(component.validRequest()).toBe(true);
-    // expect(component.orgName).toEqual('unitTestOrg');
-    // expect(component.orgParentName).toEqual('parentUnitTestOrg');
-    component.saveAccount();
-    setTimeout(function() {
-      expect(location.path()).toBe('/users/manageContact');
-    }, 1000);
+  fit('testing of the organisations service', () => {
+    fixture = TestBed.createComponent(ManageUserAccountComponent);
+    component = fixture.debugElement.componentInstance;
+    const organisationsService = fixture.debugElement.injector.get(OrganizationsService);
+    fixture.detectChanges();
+    expect(organisationsService.OrganizationGetManageableOrganizations({}).subscribe(result => {
+      expect(result.items.length).toBeGreaterThan(0);
+    }));
   });
+  // it('should initialize successfully', () => {
+  //   var manageUserData = [];
+  //   var manageUserResponse = {
+  //     showInput: "boolean",
+  //     disabled: '',
+  //     accountData: "unitTest",
+  //     userAccountData: "unitTest",
+  //     logonId: "unitTest",
+  //     email1: "unitTest",
+  //     password: "unitTest",
+  //     passwordVerify: "unitTest",
+  //     organizationName: "unitTest",
+  //     policy: "unitTest",
+  //     emailPattern: "unitTest",
+  //     envalidEmail: "unitTest",
+  //     manageUserResponse: "unitTest",
+  //     id: "number"
+  //   }
+  //   manageUserData.push(manageUserResponse);
+  //   spyOn(usermainService, 'updateUser').and.returnValue(Promise.resolve(manageUserData));
+  //   component.ngOnInit();
+  //   setTimeout(function() {
+  //     expect(usermainService.updateUser).toHaveBeenCalled();
+  //     expect(component.updateUser.find(entry => entry.content == "unitTest")).toBeDefined;
+  //     // expect(component.parentOrgItems.find(entry => entry.content == "Root Organization")).toBeDefined;
+  //   }, 1000);
+  // });
+
+  // it('the logonId should not be assigned given the logonId is not selected', () => {
+  //   // let selection : Object = {'item' : {'selected' : false, 'content' : ''}};
+  //   component.accountCall();
+  //   expect(component.logonId).toBeNull;
+  // });
+
+  // it('should find the correct existing user when the request is valid', () => {
+  //   // expect(component.validRequest()).toBe(false);
+  //   // let org : Object = {'item' : {'selected' : true, 'content' : 'unitTestOrg'}};
+  //   // let parentOrg : Object = {'item' : {'selected' : true, 'content' : 'parentUnitTestOrg'}};
+  //   // component.onOrgSelected(org);
+  //   // expect(component.validRequest()).toBe(true);
+  //   // component.onParentOrgSelected(parentOrg);
+  //   // expect(component.validRequest()).toBe(true);
+  //   // expect(component.orgName).toEqual('unitTestOrg');
+  //   // expect(component.orgParentName).toEqual('parentUnitTestOrg');
+  //   component.saveAccount();
+  //   setTimeout(function() {
+  //     expect(location.path()).toBe('/users/manageContact');
+  //   }, 1000);
+  // });
 });
